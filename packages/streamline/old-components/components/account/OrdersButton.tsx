54f15fb3306c61dadb9b923a595419e4e30795ce
izzyzs <izsoum2024@gmail.com>
2026-04-05 16:01:52 -0400

    removed streamline

--- packages/streamline/old-components/components/account/OrdersButton.tsx
+++
@@ -1,7 +0,0 @@
-"use client"
-import { Button } from "@/components/my-button";
-import {redirect} from "next/navigation";
-
-export default function OrdersButton() {
-    return <Button onClick={() => redirect("/account")}>View Account Details</Button>
-}
